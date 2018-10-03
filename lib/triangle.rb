12053cad44d7001af35af8917ef6908c65e613cb
Benjamin Jones <bljones36@gmail.com>
2018-10-03 17:37:31 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ class Triangle
       @kind = :equilateral
     elsif @v1 == @v2 || @v2 == @v3 || @v1 == @v3
       @kind = :isosceles
-    elsif @v1 !== @v2 && @v2 !== @v3 && @v1 !== @v3
+    elsif @v1 != @v2 && @v2 != @v3 && @v1 != @v3
       @kind = :scalene
     end
       

e52980d9ca0694e81194e00b3c5890b0d4ed4342
Benjamin Jones <bljones36@gmail.com>
2018-10-03 17:24:34 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(v1, v2, v3)
+    @v1 = v1
+    @v2 = v2
+    @v3 = v3
+  end
+  
+  def kind 
+    
+  end
+  
+  class TrangleError < StandardError
+  
+  end
 end
